b72c04df8943e88fc4fb4e5f17d7c55545af74e0
Yann Colina <yann.colina@grenoble-inp.org>
2017-07-23 15:27:16 +0200

    [hyperplan] A first finished version of the script
        * Added delete function
        * The suppressed values are recalculated at the end
        * The name of the output file has to be provided

--- src/hyperplan_creator.py
+++ src/hyperplan_creator.py
@@ -1,6 +1,17 @@
 import cv2
+from utils import hsl_to_rgb
+from utils import hsl_to_hex
+import sys
 import colorsys
 import numpy as np
+import json
+
+# The output file
+if len(sys.argv) != 2:
+    print "Usage: hyperplan_creator.py <output_file_name>"
+    exit()
+
+data_file = sys.argv[1]
 
 MIN_HUE = 0
 MAX_HUE = 360
@@ -10,49 +21,97 @@ MIN_SAT = 0
 MAX_SAT = 1
 SIZE_S = 10
 
-STEP_L = 0.1
+STEP_L_SMALL  = 0.01
+STEP_L_LARGEL = 0.1
 
-WIDTH  = 200
-HEIGHT = 200
+WIDTH   = 700
+HEIGHT  = 800
 
-RETURN = 13
-RIGHT = 3
-LEFT = 2
+RETURN  = 13
+UP      = 0
+DOWN    = 1
+LEFT    = 2
+RIGHT   = 3
+DELETE = 127
 
-hues = np.linspace(MIN_HUE, MAX_HUE, SIZE_H)
+hues        = np.linspace(MIN_HUE, MAX_HUE, SIZE_H)
 saturations = np.linspace(MIN_SAT, MAX_SAT, SIZE_S)
 
+# Displays a given image
 def display_image(image):
-    cv2.namedWindow('dst_rt', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('dst_rt', WIDTH, HEIGHT)
-    cv2.imshow('dst_rt', image)
+    cv2.namedWindow('HyperplanCreator', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('HyperplanCreator', WIDTH, HEIGHT)
+    cv2.imshow('HyperplanCreator', image)
     key = cv2.waitKey(0)
     # cv2.destroyAllWindows()
     return key
 
-
-
+# Creates an image where on top sits a rectangle of the given color and on
+# bottom, lays a text which color is the one provided over a black background
 def create_image(h, s, l):
-    r, g, b = colorsys.hls_to_rgb(h, s, l)
-    bgr_image = np.zeros((HEIGHT,WIDTH,3), np.uint8)
-    bgr_image[:] = (b, g, r)
-    return bgr_image
+    r, g, b = hsl_to_rgb([h, s, l])
+    image = np.zeros((HEIGHT,WIDTH,3), np.uint8)
+    image[0:HEIGHT/2] = (b, g, r)
+    image[HEIGHT/2:] = (0,0,0)
+    cv2.putText(
+            image,
+            "Notre Rice a nous",
+            (20, int(3*HEIGHT/4.0)),
+            cv2.FONT_HERSHEY_TRIPLEX,
+            2,
+            [b,g,r])
+    return image
 
+# For a given key, increses or decreases the luminosity
+def edit_luminosity(key, l):
+    if key == RIGHT:
+        l += STEP_L_SMALL
+    elif key == LEFT:
+        l -= STEP_L_SMALL
+    elif key == UP:
+        l += STEP_L_LARGEL
+    elif key == DOWN:
+        l -= STEP_L_LARGEL
+
+    if l < 0:
+        l = 0.0
+    elif l > 1:
+        l = 1.0
+    return l
+
+def save_json(array):
+    with open(data_file, 'w') as f:
+        json.dump(array, f, indent=4)
 
 l = 0
 h = 100
-s = 0.5
+s = 0
+values = []
+removed = []
+
+def choose_luminosity(h, s, l):
+    while True:
+        image = create_image(h, s, l)
+        key = display_image(image)
+        print "Current luminosity: " + str(l)
+        if key == RETURN:
+            values.append([h, s, l])
+            break
+        elif key == DELETE:
+            if values:
+                print "The last color has been removed"
+                removed.append(values.pop())
+        l = edit_luminosity(key, l)
+    return l
+
 for h in hues:
     for s in saturations:
-        while True:
-            print "Luminosity: " + str(l)
-            image = create_image(h, s, l)
-            key = display_image(image)
-            if key == RETURN:
-                break
-            elif key == RIGHT:
-                l += STEP_L
-            elif key == LEFT:
-                l -= STEP_L
+        l = choose_luminosity(h, s, l)
 
+while removed:
+    color = removed.pop()
+    choose_luminosity(color[0], color[1], color[2])
+
+
+save_json(values)
 
