eca67b5591b274eacf933a1889e3bd0da2b14e91
Robin Vincent <robin.vincent@grenoble-inp.org>
2017-10-21 18:31:56 +0200

    Better hyperplan calibration + better color gen

--- src/getcolors.py
+++ src/getcolors.py
@@ -3,9 +3,6 @@ import utils
 import color_extractor
 from PIL import Image, ImageDraw
 import argparse
-import config
-import unittest
-import sys
 
 def colors_to_file(colors, filename, resize=150, swatchsize=20):
     """ Creates a color palette and saves it to file """
@@ -27,18 +24,22 @@ if __name__ == '__main__':
     ap.add_argument("-f", "--file", help="File path to the image", required=False)
     args = vars(ap.parse_args())
 
-    extractor = color_extractor.ColorExtractor(args["file"], num_colors=200)
+    extractor = color_extractor.ColorExtractor(args["file"], num_colors=250)
     colors = extractor.get_colors()
     fg = colors["special"]["foreground"]
     bg = colors["special"]["background"]
     others = colors["colors"]
 
     rgbcols = [utils.hex_to_rgb(others[k]) for k in others]
+
     print("\nFinal palette (" + str(len(rgbcols)) + "):")
     visual.print_palette(rgbcols, size=2)
+
     print("\Foreground color:")
     visual.print_palette([utils.hex_to_rgb(fg)], size=4)
+
     print("\Background color:")
     visual.print_palette([utils.hex_to_rgb(bg)], size=4)
+
     colors_to_file([others[k] for k in others], "palette.png")
     utils.save_json("palette.json", colors)
