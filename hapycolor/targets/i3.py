56a90443514fc5e70333877167afc66d513ad721
Yann Colina <yanncolina@gmail.com>
2018-04-23 22:46:56 +0200

    [i3] Simplified config handling behaviour

--- hapycolor/targets/i3.py
+++ hapycolor/targets/i3.py
@@ -1,19 +1,17 @@
-from . import base
-
+"""
+i3 Module
+"""
 import pathlib
-from hapycolor import config
+import re
 from hapycolor import exceptions
 from hapycolor import helpers
 from hapycolor import targets
 from hapycolor.targets import yabar
 from hapycolor.targets import wallpaper
-import contextlib
-from hapycolor import targets
-import contextlib
-import re
+from . import base
 
 
-class I3(base.Target, config.ConfigurationEditor):
+class I3(base.Target):
     """
     This class aims at altering i3's configuration file.
     Currently, it changes the color of the borders and sets the new
@@ -25,12 +23,14 @@ class I3(base.Target, config.ConfigurationEditor):
     border_variable = "$border_color"
     split_variable = "$split_color"
 
+    @staticmethod
     def is_config_initialized():
         try:
             return I3.configuration_key in I3.load_config()
         except exceptions.InvalidConfigKeyError:
             return False
 
+    @staticmethod
     def initialize_config():
         """
         Searches through the common path where is stored i3's configuration.
@@ -41,13 +41,13 @@ class I3(base.Target, config.ConfigurationEditor):
         common_paths = [pathlib.Path(p).expanduser() for p in
                         ["~/.config/i3/config", "~/.i3/config"]]
 
-        for p in common_paths:
-            if p.is_file():
-                config_path = p
+        for path in common_paths:
+            if path.is_file():
+                config_path = path
 
         if config_path is None:
-            p = helpers.input_path("Path to i3 configuration file: ")
-            if not p.is_file():
+            path = helpers.input_path("Path to i3 configuration file: ")
+            if not path.is_file():
                 raise exceptions.WrongInputError("Must be a file")
             # Seems not useful
             # if not p.is_absolute():
@@ -55,36 +55,59 @@ class I3(base.Target, config.ConfigurationEditor):
 
         I3.save_config({I3.configuration_key: config_path.as_posix()})
 
+    @staticmethod
     def compatible_os():
         return [targets.OS.LINUX]
 
+    @staticmethod
     def export(palette, image_path):
         """
         .. todo::
             Change :func:`Target.is_enabled` so that it returns False
             when not present in the configuration file instead of letting
             an exception being raised.
+
+        This class defines three main features related to i3
+        - border color definition
+        - sets the new wallpaper
+        - calls yabar with the new configuration file
+
+        .. todo::
+            Add rofi support
         """
         border_color = palette.colors[0]
         split_color = palette.colors[1]
 
-        I3.assign_border_colors()
-        I3.declare_color(I3.border_variable, border_color)
-        I3.declare_color(I3.split_variable, split_color)
+        config_path = I3.load_config()[I3.configuration_key]
+        configuration = []
+        with open(config_path, 'r') as config_file:
+            configuration = config_file.readlines()
 
+        configuration = I3.assign_border_colors(configuration)
+        configuration = I3.declare_color(configuration, I3.border_variable,
+                                         border_color)
+        configuration = I3.declare_color(configuration, I3.split_variable,
+                                         split_color)
+
+        # TODO(yann) is_enabled() should return False if not initialized,
+        # when this will be changed, remove the try/catch
         try:
-            if targets.wallpaper.Wallpaper.is_enabled():
-                I3.set_wallpaper(image_path)
+            if wallpaper.Wallpaper.is_enabled():
+                configuration = I3.set_wallpaper(configuration, image_path)
         except exceptions.InvalidConfigKeyError:
             pass
 
         try:
-            if targets.yabar.Yabar.is_enabled():
-                I3.set_yabar()
+            if yabar.Yabar.is_enabled():
+                configuration = I3.set_yabar(configuration)
         except exceptions.InvalidConfigKeyError:
             pass
 
-    def declare_color(variable, color):
+        with open(config_path, 'w') as config_file:
+            config_file.write(configuration)
+
+    @staticmethod
+    def declare_color(config, variable, color):
         """
         Associates a color to a variable
             set $variable_name <hex_color>
@@ -96,17 +119,13 @@ class I3(base.Target, config.ConfigurationEditor):
 
         hex_color = helpers.rgb_to_hex(color)
 
-        config_file = I3.load_config()[I3.configuration_key]
+        pattern = r"set +{}".format(re.escape(variable))
+        replace = "set {}    {}".format(variable, hex_color)
 
-        pattern_border = "set *{}.*".format(re.escape(variable))
-        declaration = "set {}    {}".format(variable, hex_color)
-        found = I3.replace_line(config_file, pattern_border,
-                                lambda _: declaration)
-        if not found:
-            with open(config_file, 'r+') as f:
-                f.write(declaration)
+        return I3.replace_or_add(config, pattern, replace)
 
-    def assign_border_colors():
+    @staticmethod
+    def assign_border_colors(config):
         """
         Assings the variables associated to the colors of the border to i3's
         variable: `client.focused`, which takes four colors:
@@ -122,40 +141,75 @@ class I3(base.Target, config.ConfigurationEditor):
             If `client.focused` wasn't already defined the text color will be
             set to black (#000000), otherwise, the former color will be used.
         """
-        pattern = "^client\.focused (.*)$"
-        base = "client.focused    {}    {}    {}    {}" \
+        pattern = r"client.focused +(\S+) +(\S+) +(\S+) +(\S+)"
+        replace = "client.focused    {}    {}    {}    {}" \
             .format(I3.border_variable, I3.border_variable,
                     "{}", I3.split_variable)
+        match = False
+        new_config = []
+        for line in config:
+            if re.match(pattern, line):
+                match = re.match(pattern, line)
+                new_config.append(replace.format(match.group(3)))
+                match = True
+            else:
+                new_config.append(line)
 
-        def new_line(match):
-            text_color = [e for e in match.group(1).split(' ') if e != '']
-            return base.format(text_color[2])
+        if not match:
+            new_config = [replace.format("#000000")] + config
+        return new_config
 
-        config_file = I3.load_config()[I3.configuration_key]
-        found = I3.replace_line(config_file, pattern, new_line)
-
-        if not found:
-            with open(config_file, 'r+') as f:
-                f.write(base.format('#000000'))
-
-    def set_wallpaper(image_path):
-        pattern = r".*exec .*feh.*"
-        config_file = I3.load_config()[I3.configuration_key]
+    @staticmethod
+    def set_wallpaper(config, image_path):
+        """
+        If the target "wallpaper" is set, this method will insert/replace feh's
+        command in order to update the current wallpaper.
+        """
+        pattern = r".*exec .+feh"
         command = "exec --no-startup-id feh --bg-fill --no-xinerama {}" \
             .format(image_path)
-        found = I3.replace_line(config_file, pattern, lambda _: command)
+        return I3.replace_or_add(config, pattern, command)
 
-        if not found:
-            with open(config_file, 'r+') as f:
-                f.write(command)
-
-    def set_yabar():
-        pattern = r".*status_command *yabar.*"
+    @staticmethod
+    def set_yabar(config):
+        # TODO(romain|yann): Naming? hapy.conf / hapycolor.conf / hapycolor.config?
+        """
+        If the target "yabar" is enables, this method will insert/replace
+        yabar's command with the generated configuration file: `hapy.conf`
+        """
+        pattern = r".*status_command +yabar"
         config_key = yabar.Yabar.configuration_key
         config_file = pathlib.Path(yabar.Yabar.load_config()[config_key]).parent / "hapy.conf"
         command = "status_command yabar -c {}".format(config_file.as_posix())
 
-        found = I3.replace_line(config_file, pattern, lambda _: command)
-        if not found:
-            with open(config_file, 'r+') as f:
-                f.write(command)
+        return I3.replace_or_add(config, pattern, command)
+
+    @staticmethod
+    def replace_or_add(config, pattern, replace):
+        """
+        Handy method that loops through a list of strings and if a line
+        matches the provided pattern, it will replace it with the provided
+        string. If no match is found, the `replace` argument will be added
+        at the begining of the list.
+
+        :arg config: The current configuration file loaded as a list of
+            strings.
+        :arg pattern: A raw string defining a regular expression.
+        :arg replace: The new string which will be used to replace the matched
+            string.
+
+        :see: `https://stackoverflow.com/questions/2081640/what-exactly-do-u-\
+                and-r-string-flags-do-and-what-are-raw-string-literals`
+        """
+        new_config = []
+        match = False
+        for line in config:
+            if re.match(pattern, line):
+                new_config.append(replace)
+                match = True
+            else:
+                new_config.append(line)
+
+        if not match:
+            new_config = [replace] + config
+        return new_config
--- tests/test_i3.py
+++ tests/test_i3.py
@@ -1,187 +1,151 @@
+"""
+i3 Test Module
+"""
 import unittest
-import re
-import pathlib
-import os
-import contextlib
 from unittest import mock
-from unittest.mock import Mock
-from unittest.mock import MagicMock
-import tests.helpers
-from hapycolor import config
-from hapycolor import helpers
+import contextlib
 from hapycolor import exceptions
 from hapycolor import targets
+from hapycolor.targets import yabar
 from hapycolor.targets.i3 import I3
 
 class TestI3(unittest.TestCase):
-
-    @contextlib.contextmanager
-    def i3context(in_list, out_list):
-        mock_file = mock.Mock()
-        mock_file.write = lambda string: out_list.append(string)
-
-        @contextlib.contextmanager
-        def mock_open(file, mode):
-            yield mock_file
-
-        @contextlib.contextmanager
-        def mock_edit_config(file):
-            yield (in_list, out_list)
-
-        load_config = "hapycolor.targets.i3.I3.load_config"
-        i3_config = {I3.configuration_key: "tests/test_i3.txt"}
-        edit_config = "hapycolor.config.ConfigurationEditor.edit_config"
-        wallpaper_enabled = "hapycolor.targets.wallpaper.Wallpaper.is_enabled"
-        yabar_enabled = "hapycolor.targets.yabar.Yabar.is_enabled"
-        with mock.patch(load_config, return_value=i3_config),\
-                mock.patch(edit_config, mock_edit_config), \
-                mock.patch("builtins.open", mock_open), \
-                mock.patch(wallpaper_enabled, return_value=True), \
-                mock.patch(yabar_enabled, return_value=False):
-            yield
-
     def test_declare_color_colors_no_rgb(self):
-        result = []
-        with self.assertRaises(exceptions.ColorFormatError), \
-            TestI3.i3context([], result):
-            I3.declare_color(I3.border_variable, "#AAbbcc")
-            I3.declare_color(I3.split_variable, "0xFFFFFF")
+        with self.assertRaises(exceptions.ColorFormatError):
+            I3.declare_color([], I3.border_variable, "#AAbbcc")
+            I3.declare_color([], I3.split_variable, "0xFFFFFF")
 
-    def test_declare_color_colors_first(self):
-        text = ["first line", "second line", "third line", "fourth line"]
-        result = []
-        with TestI3.i3context(text, result):
-            I3.declare_color(I3.border_variable, (250, 250, 250))
-            I3.declare_color(I3.split_variable, (12, 0, 1))
+    def test_declare_color_not_set(self):
+        config = ["first line", "second line", "third line", "fourth line"]
+        result = I3.declare_color(config, I3.border_variable, (250, 250, 250))
 
-        res1 = "set {}    #fafafa".format(I3.border_variable)
-        res2 = "set {}    #0c0001".format(I3.split_variable)
-        self.assertEqual(result[9], res2)
-        self.assertEqual(result[4], res1)
+        expected = "set {}    #fafafa".format(I3.border_variable)
+        self.assertEqual(result[0], expected)
 
-    def test_declare_color_colors_redeclare_one(self):
-        text = ["first line", "second line", "third line", "fourth line",
-                "set {}    #00FF00".format(I3.split_variable), "Last line"]
-        result = []
-        with TestI3.i3context(text, result):
-            I3.declare_color(I3.border_variable, (250, 250, 250))
-            I3.declare_color(I3.split_variable, (12, 0, 1))
+    def test_declare_color_set(self):
+        config = ["first line", "second line", "third line", "fourth line",
+                  "set {}    #00FFFF".format(I3.border_variable),
+                  "set {}    #00FF00".format(I3.split_variable), "Last line"]
+        result = I3.declare_color(config, I3.split_variable, (12, 0, 1))
 
-        res1 = "set {}    #fafafa".format(I3.border_variable)
-        res2 = "set {}    #0c0001".format(I3.split_variable)
-        self.assertEqual(result[11], res2)
-        self.assertEqual(result[6], res1)
+        expected = "set {}    #0c0001".format(I3.split_variable)
+        self.assertEqual(result[5], expected)
 
-    def test_declare_color_colors_redeclare_both(self):
-        text = ["first line", "second line", "third line", "fourth line",
-                "set {}    #FF00FF".format(I3.split_variable),
-                "set {}    #123456".format(I3.border_variable), "LastLine"]
+    def test_assign_border_colors_not_set(self):
+        config = ["first line", "second line", "third line", "fourth line"]
 
-        result = []
-        with TestI3.i3context(text, result):
-            I3.declare_color(I3.border_variable, (250, 250, 250))
-            I3.declare_color(I3.split_variable, (12, 0, 1))
-
-        res1 = "set {}    #fafafa".format(I3.border_variable)
-        res2 = "set {}    #0c0001".format(I3.split_variable)
-        self.assertEqual(result[11], res2)
-        self.assertEqual(result[5], res1)
-
-    def test_assign_border_colors_first_time(self):
-        text = ["first line", "second line", "third line", "fourth line"]
-        result = []
-        with TestI3.i3context(text, result):
-            I3.assign_border_colors()
-
+        result = I3.assign_border_colors(config)
         expected = "client.focused    {}    {}    #000000    {}" \
                 .format(I3.border_variable, I3.border_variable,
                         I3.split_variable)
-        self.assertEqual(result[4], expected)
+        self.assertEqual(result[0], expected)
 
-    def test_assign_border_colors_second_time(self):
-        text = ["first line", "second line", "third line", "fourth line",
-                "client.focused    #bbbbbb    #cccccc    #ababab    #ffffff"]
-        result = []
-        with TestI3.i3context(text, result):
-            I3.assign_border_colors()
+    def test_assign_border_color_set(self):
+        config = ["first line", "second line", "third line", "fourth line",
+                  "client.focused    #bbbbbb    #cccccc    #aaaaaa    #ffffff",
+                  "Last line"]
+        result = I3.assign_border_colors(config)
 
-        not_expected = "client.focused    #bbbbbb    #cccccc    #ababab    #ffffff"
-        expected = "client.focused    {}    {}    #ababab    {}" \
+        expected = "client.focused    {}    {}    #aaaaaa    {}" \
                 .format(I3.border_variable, I3.border_variable,
                         I3.split_variable)
-        self.assertNotEqual(result[4], not_expected)
-        self.assertEqual(result[4], expected)
-
-    def test_assign_border_colors_second_time_not_last_line(self):
-        text = ["first line", "second line", "third line", "fourth line",
-                "client.focused    #bbbbbb    #cccccc    #bbbbbb    #ffffff",
-                "Last line"]
-        result = []
-
-        with TestI3.i3context(text, result):
-            I3.assign_border_colors()
-
-        not_expected = "client.focused    #bbbbbb    #cccccc    #bbbbbb    #ffffff"
-        expected = "client.focused    {}    {}    #bbbbbb    {}" \
-                .format(I3.border_variable, I3.border_variable,
-                        I3.split_variable)
-        self.assertNotEqual(result[4], not_expected)
-        self.assertEqual(result[4], expected)
-
-    def test_set_wallpaper(self):
-        text = ["first line", "second line", "third line", "fourth line",
-                "exec --no-startup-id feh --bg-fill --no-xinerama \
-                ~/Pictures/wallpapers/0JSyoDH.jpg", "Last line"]
-        result = []
-        with TestI3.i3context(text, result):
-            I3.set_wallpaper("/path/to/image.jpg")
-
-        not_expected = "exec --no-startup-id feh --bg-fill --no-xinerama " \
-                + "~/Pictures/wallpapers/0JSyoDH.jpg"
-        expected = "exec --no-startup-id feh --bg-fill --no-xinerama " \
-                + "/path/to/image.jpg"
-        self.assertNotEqual(result[4], not_expected)
         self.assertEqual(result[4], expected)
 
     @mock.patch("hapycolor.targets.yabar.Yabar.load_config",
-        return_value={targets.yabar.Yabar.configuration_key: "test.conf"})
-    def test_set_yabar(self, mock_config):
-        text = ["first line", "second line", "third line", "fourth line",
-                "status_command yabar"]
-        result = []
-        with TestI3.i3context(text, result):
-            I3.set_yabar()
+                return_value={yabar.Yabar.configuration_key: "/path/to/yabar.conf"})
+    def test_set_yabar_not_set(self, mock_config):
+        config = ["first line", "second line", "third line", "fourth line"]
+        result = I3.set_yabar(config)
 
-        expected = "status_command yabar -c hapy.conf"
+        expected = "status_command yabar -c /path/to/hapy.conf"
+        self.assertEqual(result[0], expected)
+
+    @mock.patch("hapycolor.targets.yabar.Yabar.load_config",
+                return_value={yabar.Yabar.configuration_key: "/path/to/yabar.conf"})
+    def test_set_yabar_set(self, mock_config):
+        config = ["first line", "second line", "third line", "fourth line",
+                  "status_command yabar"]
+        result = I3.set_yabar(config)
+
+        expected = "status_command yabar -c /path/to/hapy.conf"
         self.assertEqual(result[4], expected)
 
     def test_is_config_initialized_default(self):
         self.assertFalse(I3.is_config_initialized())
 
-    def test_export_only_border_empty_config(self):
-        mm = Mock()
-        mm.colors = [(100, 100, 100), (200, 200, 200)]
+    def test_declare_variable_not_set(self):
+        border_color = (100, 100, 100)
+        config = ["line 1", "line 2"]
+        result = I3.declare_color(config, I3.border_variable, border_color)
+        expected = "set $border_color    #646464"
+        self.assertEqual(result[0], expected)
+
+    def test_declare_variable_set(self):
+        border_color = (100, 100, 100)
+        config = ["line 1", "set $border_color #123455", "line 3"]
+        result = I3.declare_color(config, I3.border_variable, border_color)
+        expected = "set $border_color    #646464"
+        self.assertEqual(result[1], expected)
+    def test_export_wallpaper_not_set(self):
         result = []
-        with TestI3.i3context([], result):
-            I3.export(mm, "/path/to/image.png")
-
-        expected = [ "client.focused    $border_color    $border_color    "
-                        + "#000000    $split_color",
-                    "set $border_color    #646464",
-                    "set $split_color    #c8c8c8",
-                    "exec --no-startup-id feh --bg-fill --no-xinerama "
-                        + "/path/to/image.png"]
-        self.assertEqual(result, expected)
-
-    @mock.patch("hapycolor.targets.wallpaper.Wallpaper.is_enabled",
-                returns_value=True)
-    def test_export_wallpaper(self, mock_enabled):
-        mm = Mock()
-        mm.colors = [(100, 100, 100), (200, 200, 200)]
-
-        result = []
-        with TestI3.i3context([], result):
-            I3.set_wallpaper("/path/to/image.png")
+        config = ["line 1", "line 2"]
+        result = I3.set_wallpaper(config, "/path/to/image.png")
 
         expected = "exec --no-startup-id feh --bg-fill --no-xinerama /path/to/image.png"
         self.assertEqual(result[0], expected)
+
+    def test_export_wallpaper_set(self):
+        result = []
+        config = ["line 1", "exec --no-startup-id feh --bg-fill --no-xinerama \
+                  /path/to/otherimage.png",
+                  "line 3"]
+        result = I3.set_wallpaper(config, "/path/to/image.png")
+
+        expected = "exec --no-startup-id feh --bg-fill --no-xinerama /path/to/image.png"
+        self.assertEqual(result[1], expected)
+
+    def test_export_set(self):
+        config = ["line 1", "client.focused    #123456 #999999   "
+                  + "#aaaaaa    $split_color",
+                  "line 3",
+                  "set $border_color    #bbbbbb",
+                  "set $var_color    #aaaaaa",
+                  "status_command yabar -c /path/to/nothapy.conf",
+                  "set $split_color    #cccccc",
+                  "exec --no-startup-id feh --bg-fill --no-xinerama "
+                  + "/path/to/otherimage.png", "last line"]
+
+        expected = ["line 1", "client.focused    $border_color   "
+                    + " $border_color    #aaaaaa    $split_color",
+                    "line 3",
+                    "set $border_color    #646464",
+                    "set $var_color    #aaaaaa",
+                    "status_command yabar -c /path/to/hapy.conf",
+                    "set $split_color    #c8c8c8",
+                    "exec --no-startup-id feh --bg-fill --no-xinerama "
+                    + "/path/to/image.png", "last line"]
+
+        result = []
+        mock_file = mock.Mock()
+        mock_file.write = lambda string: result.extend(string)
+        mock_file.readlines = lambda: config
+
+        @contextlib.contextmanager
+        def mock_open(file, mode):
+            yield mock_file
+
+        palette = mock.Mock()
+        palette.colors = [(100, 100, 100), (200, 200, 200)]
+
+        mock_yabar = {yabar.Yabar.configuration_key: "/path/to/yabar.conf"}
+        with mock.patch("builtins.open", mock_open), \
+                mock.patch("hapycolor.targets.i3.I3.load_config"), \
+                mock.patch("hapycolor.targets.wallpaper.Wallpaper.is_enabled", \
+                        return_value=True), \
+                mock.patch("hapycolor.targets.yabar.Yabar.is_enabled", \
+                        return_value=True), \
+                mock.patch("hapycolor.targets.yabar.Yabar.load_config",
+                        return_value=mock_yabar):
+            I3.export(palette, "/path/to/image.png")
+
+        self.assertEqual(result, expected)
